66d2741deb4d6d833afc1439e946223cd5b38974
Nelson <nelson.ash@outlook.com>
2020-02-18 14:20:43 -0300

    Opibus -Commit inicial

---
+++ 0_Domain/Opibus.Domain.Tests/Entities/ContaTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace Opibus.Domain.Tests
+{
+    public class ContaTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AbrirConta()
+        {
+            var cliente = "123.456.789-00";
+            var valor = 10.00;
+            Assert.Pass();
+        }
+    }
+}
