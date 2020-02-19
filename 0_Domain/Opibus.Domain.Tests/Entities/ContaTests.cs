f9170c72e54fa1e8577e78e93cd60e9ef2819848
Nelson <nelson.ash@outlook.com>
2020-02-19 09:37:53 -0300

    Conta Tests

--- 0_Domain/Opibus.Domain.Tests/Entities/ContaTests.cs
+++ 0_Domain/Opibus.Domain.Tests/Entities/ContaTests.cs
@@ -13,7 +13,7 @@ namespace Opibus.Domain.Tests
         [Test]
         public void AbrirConta()
         {
-            var cliente = "123.456.789-00";
+            var cliente = new Cliente("Marcos Silva", "123.456.789-00");
             var valor = 10.00;
             var conta = new Conta(cliente, valor);
             Assert.AreEqual(conta.Saldo, 10.00);
--- 0_Domain/Opibus.Domain/Entities/Conta.cs
+++ 0_Domain/Opibus.Domain/Entities/Conta.cs
@@ -4,21 +4,13 @@ namespace Opibus.Domain.Entities
 {
     public class Conta
     {
-        private Cliente cliente { get; private set; }
-        [Obsolete]
-        public Conta(string cliente, double valor)
+
+        public Conta(Cliente cliente, double valor)
         {
             Cliente = cliente;
             Valor = valor;
         }
-
-        public Conta(Cliente cliente, double valor)
-        {
-            this.cliente = cliente;
-            Valor = valor;
-        }
-
-        public string Cliente { get; private set; }
+        public Cliente Cliente { get; private set; }
         public double Valor { get; private set; }
         public double Saldo { get { return Valor; } }
     }
